eb928fc877056c5c4510faef8248758b6a2f9091
owoborode oluwaseye <owoborodeseye@gmail.com>
2023-10-28 14:10:29 +0100

    fix issue with linting not supporting import without use

--- alembic/env.py
+++ alembic/env.py
@@ -1,3 +1,4 @@
+"""Configures the Alembic environment."""
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -6,7 +7,6 @@ from sqlalchemy import pool
 from alembic import context
 
 from app.api import models
-from app.core.config import settings
 from app.database.connection import get_db_engine, Base
 
 # this is the Alembic Config object, which provides
@@ -38,18 +38,16 @@ target_metadata = Base.metadata
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
 
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
+    This configures the context with just a URL and not an Engine,
+    though an Engine is acceptable here as well.  By skipping the Engine
+    creation we don't even need a DBAPI to be available.
 
-    Calls to context.execute() here emit the given string to the
-    script output.
-
+    Calls to context.execute() here emit the given string to the script
+    output.
     """
     url = config.get_main_option(
-        "sqlalchemy.url", 
-        get_db_engine().url.__to_string__(hide_password=False))
+        "sqlalchemy.url", get_db_engine().url.__to_string__(hide_password=False)
+    )
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -64,9 +62,8 @@ def run_migrations_offline() -> None:
 def run_migrations_online() -> None:
     """Run migrations in 'online' mode.
 
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
-
+    In this scenario we need to create an Engine and associate a
+    connection with the context.
     """
     connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),
@@ -75,9 +72,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
