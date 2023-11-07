b198709a08d2243d4aa2883209ed0e0ab7bb6f0b
Tolu Joel <toluisjoel@gmail.com>
2023-11-07 18:47:52 +0100

    Add service to verify password

--- app/services/account_services.py
+++ app/services/account_services.py
@@ -13,3 +13,17 @@ def hash_password(password: str) -> str:
         str: The hashed password.
     """
     return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> str:
+    """
+    Verify a plain password against a hashed password.
+
+    Args:
+        plain_password (str): The plain password to verify.
+        hashed_password (str): The hashed password to compare against.
+
+    Returns:
+        str: The result of the password verification.
+    """
+
+    return pwd_context.verify(plain_password, hashed_password)
