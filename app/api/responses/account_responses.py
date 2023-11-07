c7c9edf11441c45794a65061edebacc4fdf6577c
Tolu Joel <toluisjoel@gmail.com>
2023-11-07 18:24:38 +0100

    feat: Implement user signup endpoint
    - Added a new route for user signup in the /auth/signup/ URL.
    - Validates user input with the AccountSchema.
    - Checks for matching passwords before creating a new user.
    - Hashes the user's password securely using bcrypt.
    - Creates a new user in the database with unique identifiers and timestamps.
    - Returns the user's profile data in the response.

---
+++ app/api/responses/account_responses.py
@@ -0,0 +1,37 @@
+from uuid import uuid4
+from sqlalchemy.orm import Session
+from app.api.models.account_models import Account
+from app.api.schemas.account_schemas import AccountSchema, AccountResponse
+from app.services.account_services import hash_password
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from app.database.connection import get_db
+
+BASE_URL = "/auth"
+
+router = APIRouter(prefix=BASE_URL)
+
+
+@router.post("/signup/")
+def signup(
+    user: AccountSchema,
+    db: Session = Depends(get_db),
+    status_code=status.HTTP_201_CREATED,
+    response_model=AccountResponse
+) -> AccountResponse:
+    if user.password != user.confirm_password:
+        raise HTTPException(status_code=400, detail="Passwords do not match")
+
+    user_data = user.model_dump()
+    user_data["password_hash"] = hash_password(user_data["password"])
+    del user_data["password"]
+    del user_data["confirm_password"]
+    user_data["id"] = str(uuid4())
+    
+    new_user = Account(**user_data)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    
+    return new_user
+
